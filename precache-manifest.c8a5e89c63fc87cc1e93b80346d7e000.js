ed0ec4dbc977d11a608a6b9ec67600615460f9b3
rutvik2012 <rutvik.surani2012@gmail.com>
2020-05-18 22:13:20 +0530

    Updates

---
+++ precache-manifest.c8a5e89c63fc87cc1e93b80346d7e000.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e97edba889c42be6d420204ae257cb",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "feb6f3d25940ec1f045f",
+    "url": "/ContactManager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "667f5fb70b0baa48aa6a",
+    "url": "/ContactManager/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "feb6f3d25940ec1f045f",
+    "url": "/ContactManager/static/js/2.b34fb035.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.b34fb035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667f5fb70b0baa48aa6a",
+    "url": "/ContactManager/static/js/main.0e259c04.chunk.js"
+  },
+  {
+    "revision": "685e1f61bc40d23f2b05",
+    "url": "/ContactManager/static/js/runtime-main.a16fad45.js"
+  }
+]);
